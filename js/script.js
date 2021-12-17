5596ea1dabd019f439fb2dd09d89ea5075f404f4
Olya Ocheretko <olyaocheretko@gmail.com>
2021-12-17 13:22:34 +0200

    JS_task 13: add edit and remove functionallity

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const url = 'https://jsonplaceholder.typicode.com/todos'
 const requiredUserIds = [2, 4, 6]
 const requiredToDoListItems = 5
+let filteredToDoList = []
 
 window.onload = function() {
   showToDoList(url)
@@ -24,7 +25,7 @@ function showToDoList(url) {
     if(event.target.readyState === 4) {
       let toDoList = this.response
 
-      let filteredToDoList = getFilteredArray(toDoList, requiredUserIds, requiredToDoListItems)
+      filteredToDoList = getFilteredArray(toDoList, requiredUserIds, requiredToDoListItems)
       renderToDoList(filteredToDoList)
     }
   }
@@ -40,9 +41,12 @@ function getFilteredArray(toDoList, requiredUserIds, requiredToDoListItems) {
 }
 
 function renderToDoList(filteredArray) {
+  clearToDos()
+
   let container = document.createElement('div')
   document.body.append(container)
   container.className = `container`
+  container.id = 'containerToDos'
 
   filteredArray.forEach(function (userGroup) {
     let userId = userGroup[0].userId
@@ -63,48 +67,122 @@ function renderToDoList(filteredArray) {
       let olItem = document.querySelector(`.list__body-${userId}`)
       let liLast = document.createElement('li')
       liLast.className = 'list__row'
+      liLast.id = `li-${userId}_${userGroup[i].id}`
       liLast.innerHTML = `
-        <input type="text" disabled="disabled" class="list__input list__column" value="${userGroup[i].title}">
+        <input type="text" disabled="disabled" class="list__input list__column" id="input-${userGroup[i].id}" value="${userGroup[i].title}">               
         <div class="list__buttons list__column">
-            <a href="#" class="list__button list__button-med btn _btn-remove">Remove</a>
-            <a href="#" class="list__button list__button-med btn _btn-edit">Edit</a>
+            <a href="#" class="list__button list__button-med btn _btn-remove" data-id="${userGroup[i].id}">Remove</a>  
+            <a href="#" class="list__button list__button-med btn _btn-edit" data-id="${userGroup[i].id}">Edit</a>       
         </div>
       `
       olItem.append(liLast)
     }
 
-    showNewItemBlock()
-    editToDo()
+    addNewItemOnClick()
+    addEditToDoOnClick()
+    addRemoveToDoOnClick()
   })
 }
 
-function showNewItemBlock() {
+function clearToDos() {
+  let containerToDos = document.getElementById('containerToDos')
+  if (containerToDos) {
+    containerToDos.remove()
+  }
+}
+
+function addNewItemOnClick() {
   const buttons = document.querySelectorAll('.list__button')
 
-  buttons.forEach (function (button) {
-    for (let index = 0; index < buttons.length; index++) {
-      let button = buttons[index]
-
-      button.onclick = function(event) {
-        event.preventDefault();
-        this.nextElementSibling.classList.toggle('_active')
-      }
+  buttons.forEach(function (button) {
+    button.onclick = function (event) {
+      event.preventDefault();
+      this.nextElementSibling.classList.toggle('_active')
     }
-  })
+  })//TODO: realise onclick on Add button
 }
 
-function editToDo() {
+function addEditToDoOnClick() {
   const editButtons = document.querySelectorAll('._btn-edit')
 
   editButtons.forEach (function (editButton) {
     editButton.onclick = function (event) {
       event.preventDefault();
-      let activeInput = editButton.parentElement.previousElementSibling
+      let itemId = this.dataset.id
+      let input = document.getElementById(`input-${itemId}`)
 
-      if (activeInput.disabled) {
-        activeInput.disabled = ''
-        activeInput.classList.add('_active')
+      if (input.disabled) {
+        input.disabled = ''
+        input.classList.add('_active')
+        input.focus()
+      }
+      else {
+        input.disabled = 'disable'
+        input.classList.remove('_active')
+        let item = {
+          title: input.value
+        }
+
+        updateToDoItem(itemId, item)
       }
     }
   })
 }
+
+function updateToDoItem(itemId, item) {
+  const url = `https://jsonplaceholder.typicode.com/todos/${itemId}`
+  let result
+
+  fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: `${item.title}`
+    })
+  })
+      .then((response) => response.json())
+      .then((response) => result = response)
+}
+
+function addRemoveToDoOnClick() {
+  const removeButtons = document.querySelectorAll('._btn-remove')
+
+  removeButtons.forEach (function (removeButton) {
+    removeButton.onclick = function (event) {
+      event.preventDefault();
+      let itemId = this.dataset.id
+
+      deleteToDoItem(itemId)
+    }
+  })
+}
+
+function deleteToDoItem(itemId) {
+  const url = `https://jsonplaceholder.typicode.com/todos/${itemId}`
+  fetch(url, {
+    method: 'DELETE'
+  })
+      .then((response) => response.json())
+      .then(() => {
+        filteredToDoList = deleteToDoItemFromFilteredArray(filteredToDoList, itemId)
+        renderToDoList(filteredToDoList)
+      })
+}
+
+function deleteToDoItemFromFilteredArray(groupedToDoList, itemId) {
+  groupedToDoList.forEach (function (userGroup) {
+    userGroup.forEach (function (toDoItem, index) {
+      if (toDoItem.id === +itemId) {
+        userGroup.splice(index,1)
+      }
+    })
+  })
+  return groupedToDoList
+}
+
+
+
+
+
