94a186fed3b105febe9df535591647493aab5622
Olya Ocheretko <olyaocheretko@gmail.com>
2021-12-18 16:57:55 +0200

    JS_task 13: add addItem functionallity and code structure changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const url = 'https://jsonplaceholder.typicode.com/todos'
 const requiredUserIds = [2, 4, 6]
-const requiredToDoListItems = 5
+const requiredToDoListItemsQuantity = 5
 let filteredToDoList = []
 
 window.onload = function() {
@@ -25,24 +25,21 @@ function showToDoList(url) {
     if(event.target.readyState === 4) {
       let toDoList = this.response
 
-      filteredToDoList = getFilteredArray(toDoList, requiredUserIds, requiredToDoListItems)
+      filteredToDoList = getFilteredArray(toDoList, requiredUserIds, requiredToDoListItemsQuantity)
       renderToDoList(filteredToDoList)
     }
   }
 }
 
-function getFilteredArray(toDoList, requiredUserIds, requiredToDoListItems) {
+function getFilteredArray(toDoList, requiredUserIds, requiredToDoListItemsQuantity) {
   let filteredArrayOfUsers = []
   for(let i = 0; i < requiredUserIds.length; i++) {
-    filteredArrayOfUsers.push(toDoList.filter(item => item.userId === requiredUserIds[i]).slice(0,requiredToDoListItems))
+    filteredArrayOfUsers.push(toDoList.filter(item => item.userId === requiredUserIds[i]).slice(0,requiredToDoListItemsQuantity))
   }
-
   return filteredArrayOfUsers
 }
 
 function renderToDoList(filteredArray) {
-  clearToDos()
-
   let container = document.createElement('div')
   document.body.append(container)
   container.className = `container`
@@ -54,52 +51,102 @@ function renderToDoList(filteredArray) {
     toDoBlock.className = `list`
     toDoBlock.innerHTML = `
       <h2 class="list__title">To-Do list for user № ${userId}</h2>
-      <a href="#" class="list__button btn">Add new Item</a>
-      <div class="list__row list__row-new">
-        <textarea class="list__input list__column" autocomplete="off"></textarea>
-        <button type="submit" class="list__button-small btn list__column">Add</button>
+      <a href="#" class="list__button btn _btn-add" data-user_id="${userId}">Add new Item</a>
+      <div class="list__row list__row-new" id="div-${userId}">
+        <textarea class="list__input list__column" autocomplete="off" id="textarea-${userId}"></textarea>
+        <button type="submit" class="list__button-small btn list__column" id="button-${userId}">Add</button>
       </div>
-      <ol class='list__body list__body-${userId}'></ol>
+      <ol class='list__body' id="list__body-${userId}"></ol>
     `
     container.append(toDoBlock)
 
-    for (let i = 0; i < userGroup.length; i++) {
-      let olItem = document.querySelector(`.list__body-${userId}`)
-      let liLast = document.createElement('li')
-      liLast.className = 'list__row'
-      liLast.id = `li-${userId}_${userGroup[i].id}`
-      liLast.innerHTML = `
-        <input type="text" disabled="disabled" class="list__input list__column" id="input-${userGroup[i].id}" value="${userGroup[i].title}">               
-        <div class="list__buttons list__column">
-            <a href="#" class="list__button list__button-med btn _btn-remove" data-id="${userGroup[i].id}">Remove</a>  
-            <a href="#" class="list__button list__button-med btn _btn-edit" data-id="${userGroup[i].id}">Edit</a>       
-        </div>
-      `
-      olItem.append(liLast)
-    }
-
+    renderUserToDoItems(userId, userGroup)
     addNewItemOnClick()
-    addEditToDoOnClick()
-    addRemoveToDoOnClick()
   })
 }
 
-function clearToDos() {
-  let containerToDos = document.getElementById('containerToDos')
-  if (containerToDos) {
-    containerToDos.remove()
+function renderToDoItems(userGroup, userId) {
+  for (let i = 0; i < userGroup.length; i++) {
+    let olItem = document.getElementById(`list__body-${userId}`)
+    let liLast = document.createElement('li')
+    liLast.className = `list__row list__item-${userId}`
+    liLast.innerHTML = `
+        <input type="text" disabled="disabled" class="list__input list__column" id="input-${userGroup[i].id}" value="${userGroup[i].title}">               
+        <div class="list__buttons list__column">
+            <a href="#" class="list__button list__button-med btn _btn-remove" data-id="${userGroup[i].id}" data-userId="${userId}">Remove</a>  
+            <a href="#" class="list__button list__button-med btn _btn-edit" data-id="${userGroup[i].id}">Edit</a>       
+        </div>
+    `
+    olItem.append(liLast)
   }
 }
 
 function addNewItemOnClick() {
-  const buttons = document.querySelectorAll('.list__button')
+  const buttons = document.querySelectorAll('._btn-add')
 
   buttons.forEach(function (button) {
     button.onclick = function (event) {
       event.preventDefault();
-      this.nextElementSibling.classList.toggle('_active')
+      let userId = +this.dataset.user_id
+      let div = document.getElementById(`div-${userId}`)
+      let textarea =  document.getElementById(`textarea-${userId}`)
+      let addButton =  document.getElementById(`button-${userId}`)
+      let userGroup = filteredToDoList[requiredUserIds.indexOf(+userId)]
+
+      if(div.classList.contains('_active')) {
+        div.classList.remove('_active')
+      }
+      else {
+        div.classList.add('_active')
+        addNewToDoOnClick(userId, addButton, textarea, userGroup)
+      }
     }
-  })//TODO: realise onclick on Add button
+  })
+}
+
+function addNewToDoOnClick(userId, addButton, textarea, userGroup) {
+  addButton.onclick = function (event) {
+    event.preventDefault();
+    let item = {
+      userId: userId,
+      id: userGroup.length + 1,
+      title: textarea.value,
+      completed: false
+    }
+    userGroup.unshift(item)
+    addToDoItem(item, userGroup, userId)
+  }
+}
+
+function addToDoItem(item, userGroup, userId) {
+  const url = `https://jsonplaceholder.typicode.com/todos`
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userId: item.userId,
+      id: item.id,
+      title: item.title,
+      completed: item.completed
+    })
+  })
+      .then((response) => response.json())
+      .then(() => {
+        renderUserToDoItems(userId, userGroup)
+      })
+}
+
+function clearToDoItems(userId) {
+  let toDos = document.querySelectorAll(`.list__item-${userId}`)
+
+  toDos.forEach(function (toDo) {
+    if (toDo) {
+      toDo.remove()
+    }
+  })
 }
 
 function addEditToDoOnClick() {
@@ -139,7 +186,7 @@ function updateToDoItem(itemId, item) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      title: `${item.title}`
+      title: item.title
     })
   })
       .then((response) => response.json())
@@ -153,35 +200,45 @@ function addRemoveToDoOnClick() {
     removeButton.onclick = function (event) {
       event.preventDefault();
       let itemId = this.dataset.id
+      let userId = this.dataset.userid
 
-      deleteToDoItem(itemId)
+      deleteToDoItem(itemId, userId)
     }
   })
 }
 
-function deleteToDoItem(itemId) {
+function deleteToDoItem(itemId, userId) {
   const url = `https://jsonplaceholder.typicode.com/todos/${itemId}`
+
   fetch(url, {
     method: 'DELETE'
   })
       .then((response) => response.json())
       .then(() => {
-        filteredToDoList = deleteToDoItemFromFilteredArray(filteredToDoList, itemId)
-        renderToDoList(filteredToDoList)
+        let updatedUserGroup = deleteToDoItemFromFilteredArray(itemId)
+        renderUserToDoItems(userId, updatedUserGroup)
       })
 }
 
-function deleteToDoItemFromFilteredArray(groupedToDoList, itemId) {
-  groupedToDoList.forEach (function (userGroup) {
+function deleteToDoItemFromFilteredArray(itemId) {
+  let updatedUserGroup
+  filteredToDoList.forEach (function (userGroup) {
     userGroup.forEach (function (toDoItem, index) {
       if (toDoItem.id === +itemId) {
-        userGroup.splice(index,1)
+        userGroup.splice(index, 1)
+        updatedUserGroup = userGroup
       }
     })
   })
-  return groupedToDoList
+  return updatedUserGroup
 }
 
+function renderUserToDoItems(userId, userGroup) {
+  clearToDoItems(userId)
+  renderToDoItems(userGroup, userId)
+  addEditToDoOnClick()
+  addRemoveToDoOnClick()
+}
 
 
 
